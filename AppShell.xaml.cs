7fcd1f5a8e356280f05bfcdf43845aac71e2dde8
Alexsis24 <53233129+Alexsis24@users.noreply.github.com>
2023-03-23 15:01:36 +0200

    Init

--- AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PlanetSystem;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
---
+++ Views/StartPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace PlanetSystem.Views;
+
+public partial class StartPage : ContentPage
+{
+	public StartPage()
+	{
+		InitializeComponent();
+	}
+}
